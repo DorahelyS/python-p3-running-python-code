0ebd170b422e9627a80ca8ed1715ac22a08c2146
Dorahely Sanchez <dorahelys@gmail.com>
2023-12-10 11:25:15 -0600

    finished lab

---
+++ lib/app.py
@@ -0,0 +1,33 @@
+print("Hello World! Pass this test, please.")
+
+'''
+# is //
+# print() is console.log()
+
+#python lib/app.py to see 
+print("Hello World")
+
+# -------
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+
+# notice that it prints in a new line each time... so how do we fix???
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+
+# now what is Python Interpreter
+# this is python's version of js consolr browser 
+# simply type python
+
+# with python we use snake casing which is first_num as opposed to camel case firstNum
+# to exit do ctrl + d (js ctrl + c)
+
+# All the lessons in the Python curriculum use the pytest library for testing your Python code.
+# pytest is a Python library (the Python equivalent of an npm package) that provides a very simple and clean way to write tests.
+# You can also use the -x flag to tell pytest to stop running after the first failing test. This technique is helpful for focusing your attention on one problem at a time:
+'''
+
+''' is /* */
+'''
